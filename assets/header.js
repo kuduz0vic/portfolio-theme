621ea0d42890ca0c3f29e26d9d1e784560e2821d
Arnes <arneskuduz3@gmail.com>
2022-01-09 00:28:58 +0100

    Made the header items change colors on click

--- assets/header.js
+++ assets/header.js
@@ -1,6 +1,29 @@
+
 $(function(){
-    $("#custom-color_icon").click(function(){
-        $("header").toggleClass("custom-header_color_remove");
+    $("#custom-color_icon").delay(3500).click(function(){
+        $("#custom-header").toggleClass("custom-header_color_remove");
         $("#custom-header").toggleClass("custom-header_color_active");
     });
    });
+   $(function(){
+    $("#custom-color_icon").click(function(){
+        $("#logo-name_custom").toggleClass("custom-header_logo_remove");
+        $("#logo-name_custom").toggleClass("custom-header_logo_active");
+    });
+   });
+
+
+   $(function(){
+    $("#custom-color_icon").click(function(){
+        $("#header-icons_custom").toggleClass("custom-header_icons_remove");
+        $("#header-icons_custom").toggleClass("custom-header_icons_active");
+    });
+   });
+
+   $(function(){
+    $("#custom-color_icon").click(function(){
+        $("#header-icons_custom_cart").toggleClass("custom-header_icons_remove");
+        $("#header-icons_custom_cart").toggleClass("custom-header_icons_active");
+    });
+   });
+
